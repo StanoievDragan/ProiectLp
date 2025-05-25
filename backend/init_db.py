29cf5659052d4a877c734b262590d71525103923
Dragan <kamisugames@gmail.com>
2025-05-25 22:40:26 +0300

    feat: update product entries in database initialization script with new names and images

--- backend/init_db.py
+++ backend/init_db.py
@@ -1,16 +1,28 @@
 from app import db, Product, app
 
 with app.app_context():
-
     db.create_all()
-
 
     Product.query.delete()
 
-
     products = [
         Product(
-            name="Sporty Sneakers",
+            name="Tricou cu imprimeu",
+            price=27.99,
+            image="https://images.unsplash.com/photo-1739001402245-af41b0f740b4?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        ),
+        Product(
+            name="Tricou cu imprimeu Vintage",
+            price=27.99,
+            image="https://static.cropp.com/media/catalog/product/cache/1200/a4e40ebdc3e371adff845072e1c73f37/7/4/745DE-01X-050-1-1000304.jpg"
+        ),
+        Product(
+            name="Tricou cu imprimeu",
+            price=27.99,
+            image="https://static.cropp.com/media/catalog/product/7/1/716DE-00X-050-1-1042521.jpg"
+        ),
+        Product(
+            name="Tricou cu imprimeu",
             price=51.99,
             image="https://images.unsplash.com/photo-1542291026-7eec264c27ff?auto=format&fit=crop&w=400&q=80"
         ),
@@ -20,59 +32,42 @@ with app.app_context():
             image="https://images.unsplash.com/photo-1542291026-7eec264c27ff?auto=format&fit=crop&w=400&q=80"
         ),
         Product(
-            name="Sporty Sneakers",
+            name="Sosete sporty",
             price=51.99,
-            image="https://images.unsplash.com/photo-1542291026-7eec264c27ff?auto=format&fit=crop&w=400&q=80"
+            image="https://static.cropp.com/media/catalog/product/cache/1200/a4e40ebdc3e371adff845072e1c73f37/9/1/9132V-00X-002-1-882148.jpg"
+        ),
+        Product(
+            name="Sosete sporty cu imprimeu goofy",
+            price=51.99,
+            image="https://static.cropp.com/media/catalog/product/cache/1200/a4e40ebdc3e371adff845072e1c73f37/3/2/324DD-49X-001-1-962701_1.jpg"
+        ),
+        Product(
+            name="item",
+            price=51.99,
+            image="https://picsum.photos/200/300"
+        ),
+        Product(
+            name="Random",
+            price=51.99,
+            image="https://picsum.photos/200/300"
         ),
         Product(
             name="Sporty Sneakers",
             price=51.99,
-            image="https://images.unsplash.com/photo-1542291026-7eec264c27ff?auto=format&fit=crop&w=400&q=80"
+            image="https://picsum.photos/200/300"
         ),
         Product(
-            name="Sporty Sneakers",
+            name="Random",
             price=51.99,
-            image="https://images.unsplash.com/photo-1542291026-7eec264c27ff?auto=format&fit=crop&w=400&q=80"
+            image="https://picsum.photos/200/300"
         ),
         Product(
-            name="Sporty Sneakers",
+            name="random",
             price=51.99,
-            image="https://images.unsplash.com/photo-1542291026-7eec264c27ff?auto=format&fit=crop&w=400&q=80"
-        ),
-        Product(
-            name="Sporty Sneakers",
-            price=51.99,
-            image="https://images.unsplash.com/photo-1542291026-7eec264c27ff?auto=format&fit=crop&w=400&q=80"
-        ),
-        Product(
-            name="Sporty Sneakers",
-            price=51.99,
-            image="https://images.unsplash.com/photo-1542291026-7eec264c27ff?auto=format&fit=crop&w=400&q=80"
-        ),
-        Product(
-            name="Sporty Sneakers",
-            price=51.99,
-            image="https://images.unsplash.com/photo-1542291026-7eec264c27ff?auto=format&fit=crop&w=400&q=80"
-        ),
-        Product(
-            name="Sporty Sneakers",
-            price=51.99,
-            image="https://images.unsplash.com/photo-1542291026-7eec264c27ff?auto=format&fit=crop&w=400&q=80"
-        ),
-        Product(
-            name="Sporty Sneakers",
-            price=51.99,
-            image="https://images.unsplash.com/photo-1542291026-7eec264c27ff?auto=format&fit=crop&w=400&q=80"
-        ),
-        Product(
-            name="Sporty Sneakers",
-            price=51.99,
-            image="https://images.unsplash.com/photo-1542291026-7eec264c27ff?auto=format&fit=crop&w=400&q=80"
+            image="https://picsum.photos/200/300"
         )
 
-
     ]
-
 
     db.session.add_all(products)
     db.session.commit()
