43fac8093cbf91bc36a6d331eba41d8377b6a5ca
popes <codrut.popescu@student.upt.ro>
2025-05-25 23:24:09 +0300

    gata cu hainile

--- backend/init_db.py
+++ backend/init_db.py
@@ -24,7 +24,7 @@ with app.app_context():
         Product(
             name="Tricou cu imprimeu",
             price=51.99,
-            image="https://images.unsplash.com/photo-1542291026-7eec264c27ff?auto=format&fit=crop&w=400&q=80"
+            image="https://static.cropp.com/media/catalog/product/cache/1200/a4e40ebdc3e371adff845072e1c73f37/7/6/766DE-00X-001-1-1028743_3.jpg"
         ),
         Product(
             name="Sporty Sneakers",
@@ -47,9 +47,9 @@ with app.app_context():
             image="https://static.cropp.com/media/catalog/product/cache/850/a4e40ebdc3e371adff845072e1c73f37/2/3/233CU-09M-050-1-963830_2.jpg"
         ),
         Product(
-            name="Random",
+            name="Pantaloni Scurti emo",
             price=51.99,
-            image="https://picsum.photos/200/300"
+            image="https://static.cropp.com/media/catalog/product/cache/1200/a4e40ebdc3e371adff845072e1c73f37/3/3/330DE-99X-050-1-1021978.jpg"
         ),
         Product(
             name="Sporty Sneakers",
@@ -57,14 +57,14 @@ with app.app_context():
             image="https://picsum.photos/200/300"
         ),
         Product(
-            name="Random",
+            name="Sporty Shoes '2'",
             price=51.99,
-            image="https://picsum.photos/200/300"
+            image="https://cdn.4f.com.pl/media/catalog/product/cache/126134d464c7385b2f5a1c0d60cd5352/4/F/4FRSS25FSPOM147-20S-P-01-MAIN.jpg"
         ),
         Product(
-            name="random",
+            name="Costum catwoman",
             price=51.99,
-            image="https://picsum.photos/200/300"
+            image="https://cdn.4f.com.pl/media/catalog/product/cache/126134d464c7385b2f5a1c0d60cd5352/4/F/4FALSSS25TFTIF382-20S-M-04.jpg"
         )
 
     ]
